3ec94cab67c96cb017eda6c5957e3f779d6c525a
bryan <bryan53qm@gmail.com>
2025-12-15 16:34:31 -0500

    Enable Cors

--- src/main.ts
+++ src/main.ts
@@ -6,13 +6,20 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.setGlobalPrefix("api");
+
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
       forbidNonWhitelisted: true,
       transform: true
     })
-  )
+  );
+
+  app.enableCors({
+    origin: "http://localhost:8080",
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    credentials: true
+  });
 
   await app.listen(process.env.PORT!);
 }
